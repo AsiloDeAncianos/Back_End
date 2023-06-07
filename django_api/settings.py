36c09a2a06170090450a94e352300c62b4c81c50
Luna <vultines@gmail.com>
2023-06-07 19:49:01 -0400

    minor changes

--- django_api/settings.py
+++ django_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hngk^wucu$i8_5b6^y1qp5mv%e@2jwh7g%j9-+r_(g7wf8669w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
